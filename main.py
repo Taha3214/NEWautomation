30b74065e149065e6e32458c40130a553677f34c
Taha3214 <taha.rifkyahmed@gmail.com>
2026-06-21 10:32:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import requests
-
-response = requests.get("https://httpbin.org/get")
-
-print(response.status_code)
+print("initializing")
+response = requests.post(
+    "https://ntfy.sh/test_alerts",
+    data="Hi gm",
+    headers={
+        "Title": "Test",
+        "Priority": "1"
+    },
+    timeout=10
+)
+response.raise_for_status()
